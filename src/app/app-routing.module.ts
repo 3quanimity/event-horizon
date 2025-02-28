9e2b0d6f0a2154aa92fb9bb5bfcba9c7b2544d5c
Haythem <sinnner6@gmail.com>
2025-02-28 10:47:22 +0100

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { EventsComponent } from './features/events/events.component';
+import { GuestsComponent } from './features/guests/guests.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'guests', component: GuestsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
